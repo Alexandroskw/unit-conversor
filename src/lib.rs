34bfc855b34f48288d060fa088f140e9da608990
Alexandroskw <filipbilly.120@gmail.com>
2024-06-12 15:48:14 -0600

    Se agregan los módulos para hacer las conversiones

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
-mod length;
-mod weight;
-mod temp;
+mod length {
+    pub fn meter_to_feet(meter: f32) -> f32 {
+        meter * 3.28084
+    }
+
+    pub fn feet_to_meter(feet: f32) -> f32 {
+        feet / 3.28084
+    }
+}
+
+mod weight {
+    pub fn kg_to_pound(kg: f32) -> f32 {
+        kg * 2.20462
+    }
+
+    pub fn pound_to_kg(pound: f32) -> f32 {
+        pound / 2.20462
+    }
+}
+
+mod temp {
+    pub fn c_to_f(c: f32) -> f32 {
+        c * 1.8 + 32.0
+    }
+
+    pub fn f_to_c(f: f32) -> f32 {
+        (f - 32.0) / 1.8
+    }
+
+    pub fn c_to_k(c: f32) -> f32 {
+        c + 273.15
+    }
+
+    pub fn k_to_c(k: f32) -> f32 {
+        k - 273.15
+    }
+}
