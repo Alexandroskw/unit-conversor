d813646690881ce13938ac0c82c6d69c8229a552
Alexandroskw <filipbilly.120@gmail.com>
2024-08-16 14:38:55 -0600

    Ajustes en la sintaxis

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
-mod utils;
-mod submult;
 mod conv;
+mod submult;
+mod utils;
 
-use std::io;
-use conversor::submult::*;
 use conversor::conv::*;
+use conversor::submult::*;
+use std::io;
 use utils::{read_in, reset_program};
 
+#[allow(warnings)]
 fn main() {
     loop {
         println!("Bienvenido al conversor de unidades");
         println!("¿Qué deseas hacer?:");
         println!("1. Para hacer una conversión de  múltiplos en el S.I.\n2. Para hacer una conversión en cualquiera de ambos sistemas (Internacional, Imperial):");
 
-       match read_in().unwrap_or_else(|_| { 
+        match read_in().unwrap_or_else(|_| {
             println!("Introduce una opción");
             0
-       }) {
-           1 => {
-               internacional();
-           }
-           2 => {
-               conversor();
-           }
-           _ => {
-               println!("No es una opción válida");
-           }
-       }
+        }) {
+            1 => {
+                internacional();
+            }
+            2 => {
+                conversor();
+            }
+            _ => {
+                println!("No es una opción válida");
+            }
+        }
         if !reset_program() {
             println!("Gracias por usar el conversor");
             break;
@@ -46,35 +47,37 @@ fn internacional() {
         match read_in().unwrap_or_else(|_| {
             println!("Introduce un número");
             0
-            }) {
-                1 => {
-                    // Sección para submúltiplos de tiempo
-                    println!("Seleccionaste tiempo");
-                    println!("Introduce un número para iniciar la conversión:");
+        }) {
+            1 => {
+                // Sección para submúltiplos de tiempo
+                println!("Seleccionaste tiempo");
+                println!("Introduce un número para iniciar la conversión:");
 
-                    let mut time = String::new();
+                let mut time = String::new();
                 // Uso de tuplas para la simplicidad del programa. Al crear una tupla se debe
                 // de indicar el tipo de cada uno de los elementos (string y una función de que
                 // regresa un 'f32' en este caso)
-                    let sub:[(&str, fn(f32) -> f32); 4] = [
-                        ("minutos", tiempo::seg_to_min),
-                        ("horas", tiempo::seg_to_hr),
-                        ("milisegundos", tiempo::seg_to_ms),
-                        ("microsegundos", tiempo::seg_to_us),
-                    ];
+                let sub: [(&str, fn(f32) -> f32); 4] = [
+                    ("minutos", tiempo::seg_to_min),
+                    ("horas", tiempo::seg_to_hr),
+                    ("milisegundos", tiempo::seg_to_ms),
+                    ("microsegundos", tiempo::seg_to_us),
+                ];
 
-                    io::stdin().read_line(&mut time).expect("Error al leer la línea");
+                io::stdin()
+                    .read_line(&mut time)
+                    .expect("Error al leer la línea");
 
-                    let time: f32 = time.trim().parse().expect("Debe ser un número");
+                let time: f32 = time.trim().parse().expect("Debe ser un número");
 
-                    println!("Introdujiste: {time}");                    
-                    for &(unidad, func) in sub.iter() {   
-                        let resultado = func(time);
-                        println!("{} segundos son {} {}", time, resultado, unidad);
-                    }
-
-                    break;
+                println!("Introdujiste: {time}");
+                for &(unidad, func) in sub.iter() {
+                    let resultado = func(time);
+                    println!("{} segundos son {} {}", time, resultado, unidad);
                 }
+
+                break;
+            }
             2 => {
                 // Sección para submúltiplos de longitud
                 println!("Seleccionaste longitud");
@@ -90,7 +93,9 @@ fn internacional() {
                     ("nanómetros", longitud::m_to_nm),
                 ];
 
-                io::stdin().read_line(&mut long).expect("Error al leer la línea");
+                io::stdin()
+                    .read_line(&mut long)
+                    .expect("Error al leer la línea");
 
                 let long: f32 = long.trim().parse().expect("Debe ser un número");
 
@@ -99,7 +104,7 @@ fn internacional() {
                     let resultado = func(long);
                     println!("{} metros equivalen a {} {}", long, resultado, unidad);
                 }
-               
+
                 break;
             }
             3 => {
@@ -117,7 +122,9 @@ fn internacional() {
                     ("nanogramos", mass::g_to_ng),
                 ];
 
-                io::stdin().read_line(&mut mass).expect("Error al leer la línea");
+                io::stdin()
+                    .read_line(&mut mass)
+                    .expect("Error al leer la línea");
 
                 let mass: f32 = mass.trim().parse().expect("Debe ser un número");
                 println!("Introdujiste: {mass} gramos");
@@ -126,10 +133,10 @@ fn internacional() {
                     println!("{} gramos equivalen a {} {}", mass, resultado, unidad);
                 }
 
-               break;
+                break;
             }
             4 => {
-                println!("Seleccionaste corriente eléctrica"); 
+                println!("Seleccionaste corriente eléctrica");
                 println!("Introduce un número para la conversión:");
 
                 let mut e = String::new();
@@ -139,7 +146,9 @@ fn internacional() {
                     ("nanoamperes", ampere::a_to_na),
                 ];
 
-                io::stdin().read_line(&mut e).expect("Error al leer la línea");
+                io::stdin()
+                    .read_line(&mut e)
+                    .expect("Error al leer la línea");
 
                 let _e: f32 = e.trim().parse().expect("Debe ser un número");
                 println!("Introdujiste: {e} amperes");
@@ -149,9 +158,9 @@ fn internacional() {
                     println!("{} amperes equivalen a {} {}", _e, resultado, unidad);
                 }
                 break;
-            } 
+            }
             _ => {
-                println!("No es una opción válida"); 
+                println!("No es una opción válida");
             }
         }
     }
@@ -161,13 +170,13 @@ fn internacional() {
 fn conversor() {
     println!("Conversor de unidades");
     println!("Este es un conversor de unidades.\nSelecciona el sistema en el que quieras hacer la conversión:");
-    println!("1: Sistema Internacional a Imperial\n2: Sistema Imperial a Internacional"); 
+    println!("1: Sistema Internacional a Imperial\n2: Sistema Imperial a Internacional");
 
     let sel = match read_in() {
         Ok(valor) => valor,
         Err(_err) => {
             println!("Error al leer la línea");
-            return
+            return;
         }
     };
     if sel == 1 {
@@ -179,18 +188,16 @@ fn conversor() {
         println!("4. Convertir de Celsius a Kelvin");
         println!("5. Convertir de Kelvin a Celsius");
         conversions(sel);
-    }
-    else if sel == 2 {
+    } else if sel == 2 {
         println!("Seleccionaste el conversor del Sistema Imperial al Internacional");
         println!("¿Qué conversión quieres hacer?:");
         println!("6. Convertir de pies a metros");
         println!("7. Convertir de libras a kilos");
         println!("8. Convertir de Farenheit a Celsius");
         conversions(sel);
-    }
-    else {
+    } else {
         println!("Opción no válida.");
-    } 
+    }
 }
 
 // Función para convertir del S.I. al Imperial y viceversa
@@ -203,16 +210,16 @@ fn conversions(selection: u32) {
         1 => {
             println!("Conversor de metros [m] a pies [ft]");
             println!("Introduce el valor a convertir:");
-            
+
             let mut m = String::new();
             io::stdin().read_line(&mut m).expect("Introduce un número");
             let m = m.trim().parse::<f32>().expect("Error al leer la entrada");
             println!("{} metros equivale a {} pies", m, length::meter_to_feet(m));
-        } 
+        }
         2 => {
             println!("Conversor de kilos [kg] a libras [lb]");
             println!("Introduce el valor a convertir:");
-            
+
             let mut kg = String::new();
             io::stdin().read_line(&mut kg).expect("Introduce un número");
             let kg = kg.trim().parse::<f32>().expect("Error al leer la entrada");
@@ -223,8 +230,13 @@ fn conversions(selection: u32) {
             println!("Introduce el valor a convertir:");
 
             let mut temp = String::new();
-            io::stdin().read_line(&mut temp).expect("Introduce un número");
-            let t = temp.trim().parse::<f32>().expect("Error al leer la entrada");
+            io::stdin()
+                .read_line(&mut temp)
+                .expect("Introduce un número");
+            let t = temp
+                .trim()
+                .parse::<f32>()
+                .expect("Error al leer la entrada");
             println!("{} °C equivale a {} °F", t, temp::c_to_f(t));
         }
         4 => {
@@ -232,8 +244,13 @@ fn conversions(selection: u32) {
             println!("Introduce un valor a converir:");
 
             let mut temp = String::new();
-            io::stdin().read_line(&mut temp).expect("Introduce un número");
-            let t = temp.trim().parse::<f32>().expect("Error al leer la entrada");
+            io::stdin()
+                .read_line(&mut temp)
+                .expect("Introduce un número");
+            let t = temp
+                .trim()
+                .parse::<f32>()
+                .expect("Error al leer la entrada");
             println!("{} °C equivale a {} K", t, temp::c_to_k(t));
         }
         5 => {
@@ -241,8 +258,13 @@ fn conversions(selection: u32) {
             println!("Introduce un valor a converir:");
 
             let mut temp = String::new();
-            io::stdin().read_line(&mut temp).expect("Introduce un número");
-            let t = temp.trim().parse::<f32>().expect("Error al leer la entrada");
+            io::stdin()
+                .read_line(&mut temp)
+                .expect("Introduce un número");
+            let t = temp
+                .trim()
+                .parse::<f32>()
+                .expect("Error al leer la entrada");
             println!("{} °C equivale a {} K", t, temp::k_to_c(t));
         }
         6 => {
